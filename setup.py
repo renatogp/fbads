12c8fe45d30271de3cd250c9e28d782df31ca0bd
Renato Pedigoni <renatopedigoni@gmail.com>
2013-10-14 09:54:51 -0300

    fixed setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,26 @@
+import re
 import os
 from setuptools import setup, find_packages
 from fbads import __version__
 
 
-def READFILE(file_name):
+def read_file(file_name):
     f = open(os.path.join(os.path.dirname(__file__), file_name))
     return f.read()
+
+
+def get_version():
+    meta_filedata = read_file('fbads/__init__.py')
+    re_version = re.compile(r'VERSION\s*=\s*\((.*?)\)')
+    group = re_version.search(meta_filedata).group(1)
+    version = filter(None, map(lambda s: s.strip(), group.split(',')))
+    return '.'.join(version)
+
 
 setup(
     name='fbads',
     version=__version__,
-    description=READFILE('README.rst'),
+    description=read_file('README.rst'),
     long_description='',
     classifiers=[
         'Programming Language :: Python',
@@ -23,5 +33,5 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
-    install_requires=[d for d in READFILE('requirements.txt').split('\n') if d],
+    install_requires=[d for d in read_file('requirements.txt').split('\n') if d],
 )
